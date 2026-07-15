0f46b840e1ab9d15001959a49309653e2e5239d6
TheOGcoder <cryptoarjun9@gmail.com>
2026-07-16 02:58:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+ACTIVEPIECES_WEBHOOK = os.getenv("ACTIVEPIECES_WEBHOOK")
